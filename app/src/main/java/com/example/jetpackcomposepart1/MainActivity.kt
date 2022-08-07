ff9642de88a26a7db4dd35e5e87de0b33670f624
Hamza919 <hamza@evolvedash.com>
2022-08-07 16:53:52 +0500

    Added Custom Top Bar

--- app/src/main/java/com/example/jetpackcomposepart1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepart1/MainActivity.kt
@@ -7,11 +7,14 @@ import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposepart1.ui.theme.JetpackComposePart1Theme
 
@@ -20,15 +23,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePart1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                //Scaffold
+                CustomScaffold()
+
+
             }
         }
+    }
+}
+
+@Composable
+fun CustomScaffold() {
+    Scaffold(
+        //Top Bar
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "JetPack Compose")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Notifications, contentDescription = "Notifications")
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Search, contentDescription = "Search")
+                    }
+                }
+            )
+        }
+    ) {
+        Greeting("Android")
     }
 }
 
@@ -41,6 +70,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposePart1Theme {
-        Greeting("Android")
+        CustomScaffold()
     }
 }
