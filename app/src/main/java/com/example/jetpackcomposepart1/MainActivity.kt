c03b6b40c26846fd7f2269cc6a4f91bb9f2fb232
Hamza919 <hamza@evolvedash.com>
2022-08-09 14:45:35 +0500

    Login Screen with Column, OutlinedTextField, Text, Button

--- app/src/main/java/com/example/jetpackcomposepart1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepart1/MainActivity.kt
@@ -6,16 +6,21 @@ import android.os.Bundle
 import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposepart1.ui.theme.JetpackComposePart1Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +30,6 @@ class MainActivity : ComponentActivity() {
             JetpackComposePart1Theme {
                 //Scaffold
                 CustomScaffold()
-
 
             }
         }
@@ -57,14 +61,54 @@ fun CustomScaffold() {
             )
         }
     ) {
-        Greeting("Android")
+        loginScreen()
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun loginScreen() {
+    val userName = remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(20.dp)) {
+        Text(
+            text = "Welcome To Login Screen",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = userName.value,
+            onValueChange = {
+                userName.value = it
+            },
+            label = { Text(text = "User Name") },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = "User Icon")
+            }
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = userName.value,
+            onValueChange = {
+                userName.value = it
+            },
+            label = { Text(text = "User Name") },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = "User Icon")
+            }
+        )
+        
+        OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Button")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
