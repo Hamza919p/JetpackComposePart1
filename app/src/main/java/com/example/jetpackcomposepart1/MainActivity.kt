4f585eaef1ffdc03171c2846d3d8c3e4f1a3b040
Hamza919 <hamza@evolvedash.com>
2022-08-09 19:50:48 +0500

    RecyclerView

--- app/src/main/java/com/example/jetpackcomposepart1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepart1/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.jetpackcomposepart1
 
-import android.content.Context
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -16,12 +17,17 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposepart1.ui.theme.JetpackComposePart1Theme
+import com.example.jetpackcomposepart1.utils.User
+import com.example.jetpackcomposepart1.utils.dummyData
+import javax.sql.DataSource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,7 +67,7 @@ fun CustomScaffold() {
             )
         }
     ) {
-        loginScreen()
+        initializeRecyclerView(users = dummyData())
     }
 }
 
@@ -71,9 +77,11 @@ fun loginScreen() {
         mutableStateOf("")
     }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(20.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
         Text(
             text = "Welcome To Login Screen",
             modifier = Modifier.fillMaxWidth(),
@@ -102,13 +110,41 @@ fun loginScreen() {
                 Icon(Icons.Default.Person, contentDescription = "User Icon")
             }
         )
-        
+
         OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
             Text(text = "Button")
         }
     }
 }
 
+@Composable
+fun recyclerViewLayout(data: User) {
+    Card(shape = RoundedCornerShape(10.dp), modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 8.dp)) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.camera),
+                contentDescription = "Profile image",
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(10.dp)
+                    .size(80.dp)
+                    .clip(
+                        RoundedCornerShape(10.dp)
+                    )
+            )
+            Text(text = data.description)
+        }
+    }
+}
+
+@Composable
+fun initializeRecyclerView(users: List<User>) {
+    LazyColumn {
+        items(users) { user ->     //'users' will get data from list and pass single user to 'user'
+            recyclerViewLayout(data = user)
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
